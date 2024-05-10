467a1fe36fea1fa5e96874430766624fa232db13
MAJD <majd40098@gmail.com>
2024-05-10 15:32:11 +0300

    updated harpoon config and added tmux to vim

--- lua/majd/plugins/harpoon.lua
+++ lua/majd/plugins/harpoon.lua
@@ -4,20 +4,19 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     event = "VimEnter",
     init = function()
-        local ui = require("harpoon.ui")
-        local mark = require("harpoon.mark")
+        local harpoon = require("harpoon")
 
-        vim.keymap.set("n", "<leader>a", mark.add_file)
-        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<leader>g1", function() ui.nav_file(1) end)
-        vim.keymap.set("n", "<leader>g2", function() ui.nav_file(2) end)
-        vim.keymap.set("n", "<leader>g3", function() ui.nav_file(3) end)
-        vim.keymap.set("n", "<leader>g4", function() ui.nav_file(4) end)
-        vim.keymap.set("n", "<leader>g5", function() ui.nav_file(5) end)
-        vim.keymap.set("n", "<leader>g6", function() ui.nav_file(6) end)
-        vim.keymap.set("n", "<leader>g7", function() ui.nav_file(7) end)
-        vim.keymap.set("n", "<leader>g8", function() ui.nav_file(8) end)
-        vim.keymap.set("n", "<leader>g9", function() ui.nav_file(9) end)
+        vim.keymap.set("n", "<leader>g1", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<leader>g2", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<leader>g3", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<leader>g4", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<leader>g5", function() harpoon:list():select(5) end)
+        vim.keymap.set("n", "<leader>g6", function() harpoon:list():select(6) end)
+        vim.keymap.set("n", "<leader>g7", function() harpoon:list():select(7) end)
+        vim.keymap.set("n", "<leader>g8", function() harpoon:list():select(8) end)
+        vim.keymap.set("n", "<leader>g9", function() harpoon:list():select(9) end)
     end,
 }
